18858dba4fba7c9e3cb276e2f07174027883cae6
Krishna Bhardwaj <krishna3bhardwaj@gmail.com>
2025-04-23 16:08:40 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ mongoose.connect(DB).then((con) => {
   console.log("DB connection successful!");
 });
 
-const port = 8000;
+const port = 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
