34a4f799679321e31b7a6e2d37338374bbb499ba
Krishna Bhardwaj <krishna3bhardwaj@gmail.com>
2025-05-15 22:37:31 +0530

    router initial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import Navbar from "./components/navbar";
+import Footer from "./components/pageFooter";
+
 const App = () => {
-  return (
-    <div className="app">
-      This is just an App.
+    return <div className="flex flex-col h-full">
+      <Navbar />
+      <div className="flex-1">
+        this is main page
+      </div>
+      <Footer />
     </div>
-  );
 };
 
 export default App;
