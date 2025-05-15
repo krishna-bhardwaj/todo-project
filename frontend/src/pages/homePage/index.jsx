1f7134e9542fbb25d2050709aac0f392c84b53d3
Krishna Bhardwaj <krishna3bhardwaj@gmail.com>
2025-05-15 23:57:39 +0530

    home page, navbar and footer ui change

---
+++ frontend/src/pages/homePage/index.jsx
@@ -0,0 +1,17 @@
+import { Button } from "../../components";
+
+const HomePage = () => {
+  return (
+    <div className=" w-full h-full flex flex-col items-center justify-center bg-gray-100 text-center px-6">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Take Control of Your Day
+      </h1>
+      <p className="text-lg text-gray-600 max-w-3xl mb-6">
+        Plan, prioritize, and stay productive with our intuitive task manager. Simplify your work and personal life in one place.
+      </p>
+      <Button title={"Add Your First Task"} isbgThemeLight />
+    </div>
+  );
+};
+
+export default HomePage;
--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import HomePage from "../pages/homePage";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        Component:App
+        Component:App,
+        children: [
+            {index: true, Component:HomePage },
+        ]
     }
 ]);
 
