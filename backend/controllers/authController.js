7761994d2051fe098bfd6c234d4b82f2bd38aa93
Krishna Bhardwaj <home@91sqfts-MacBook-Pro-2.local>
2025-05-26 17:56:28 +0530

    added logout api in backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,33 +1,30 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 
-//SIGN UP
 exports.signup = async (req, res) => {
     try {
         const { email, username, password } = req.body;
         const hashedPassword = bcrypt.hashSync(password);
         const user = new User({ email, username, password: hashedPassword });
         await user.save();
-        const token = await user.generateAuthToken();
-        res.status(200).json({ user, token });
+        return res.status(200).json({ message: "Registration Successfull!" });
     } catch (error) {
         if (error?.keyPattern?.email)
-            res.status(400).json({ message: 'Email ID already registered.' });
+            return res.status(400).json({ message: 'Email ID already registered.' });
         else if (error?.keyPattern?.username)
-            res.status(400).json({ message: 'Username already exists.' });
+            return res.status(400).json({ message: 'Username already exists.' });
         else
-            res.status(500).json({ message: 'Internal server error.' });
+            return res.status(500).json({ message: 'Internal server error.' });
     }
 };
 
-//SIGN IN
 exports.login = async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.username, req.body.password);
         const token = await user.generateAuthToken();
-        res.send({user,token});
+        return res.send({user,token});
     } catch (err) {
-        res.status(err.statusCode || 500).json({ message: err.message });
+        return res.status(err.statusCode || 500).json({ message: err.message });
     }
 };
 
@@ -35,10 +32,24 @@ exports.verify = async(req, res) => {
     try {
         const token = req.header('Authorization')?.replace('Bearer ', '');
         if(!token) {
-            res.status(400).json({message:"You must login first to start adding task"});
+            return res.status(400).json({message:"No token found"});
         }
         const user = await User.findByToken(token);
-        res.send({user});
+        return res.send({user});
+    } catch (err) {
+        res.status(err.statusCode || 500).json({message: err.message});
+    }
+}
+
+exports.logout = async(req,res) => {
+    try {
+        const token = req.header('Authorization')?.replace('Bearer ', '');
+        if(!token) {
+            return res.status(400).json({message:"No token found"});
+        }
+        const user = await User.findByToken(token);
+        await user.deleteToken(token);
+        res.status(200).json({message:"Logged out succesfully"});
     } catch (err) {
         res.status(err.statusCode || 500).json({message: err.message});
     }
