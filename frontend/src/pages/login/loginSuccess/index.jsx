2e6d5d6671d81d0de5967855f12e4f4c4cb35739
Krishna Bhardwaj <home@91sqfts-MacBook-Pro-2.local>
2025-05-26 16:51:17 +0530

    proper icons added to the components

--- frontend/src/pages/login/loginSuccess/index.jsx
+++ frontend/src/pages/login/loginSuccess/index.jsx
@@ -3,22 +3,21 @@ import { APP_ROUTE } from "../../../constants";
 import { Button } from "../../../components";
 import { CheckCircle } from "lucide-react";
 
-const SignUpSucces = () => {
+const LoginSuccess = () => {
   return (
     <div className="h-full w-full flex flex-col items-center justify-center bg-gray-100 text-center px-6">
       <CheckCircle className="w-16 h-16 text-green-500 mb-4" strokeWidth={1.5} />
       <h2 className="text-3xl font-bold text-center text-[#282c34] mb-4">
-        Registration Successful!
+        You're Logged In!
       </h2>
       <p className="text-lg text-gray-600 max-w-3xl mb-6">
-        🎉 Your account has been created successfully. Welcome aboard! 
-        You can now log in and start managing your tasks efficiently.
+        🚀 Welcome back! You're successfully logged in. Head over to your dashboard to start adding and managing your tasks with ease.
       </p>
-      <Link to={APP_ROUTE.LOG_IN}>
-        <Button title="Proceed to Log In" isbgThemeLight />
+      <Link to={APP_ROUTE.TODO}>
+        <Button title="Manage Your Tasks" isbgThemeLight />
       </Link>
     </div>
   );
-}
+};
 
-export default SignUpSucces;
+export default LoginSuccess;
