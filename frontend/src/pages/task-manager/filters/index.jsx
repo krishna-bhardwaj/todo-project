4685b305f4bdd29ddfe9c478d3aba0e13867ba19
Krishna Bhardwaj <krishna3bhardwaj@gmail.com>
2025-06-25 02:57:36 +0530

    filter related changes

--- frontend/src/pages/task-manager/filters/index.jsx
+++ frontend/src/pages/task-manager/filters/index.jsx
@@ -1,4 +1,7 @@
 import { Search } from "lucide-react";
+import { TASK_STATUS } from "../../../constants";
+import { useEffect, useState } from "react";
+import { getLastActions } from "../../../utils/task";
 
 const FilterButton = ({ title, isSelected, onClick, count }) => {
   return (
@@ -19,23 +22,51 @@ const FilterButton = ({ title, isSelected, onClick, count }) => {
   );
 };
 
-const Filters = ({
-  selectedFilter,
-  setSelectedFilter,
-  searchQuery,
-  setSearchQuery,
-}) => {
-  const filters = ["Not Started", "In Progress", "Paused", "Completed"];
+const Filters = ({ filterAndSearchText, setFilterAndSearchText }) => {
+  const [selectedFilters, setSelectedFilters] = useState([]);
+
+  const taskStatuses = Object.values(TASK_STATUS);
+
+  const handleFilterClick = (title) => {
+    if (selectedFilters.includes(title)) {
+      setSelectedFilters((prev) => prev.filter((item) => item !== title));
+    } else setSelectedFilters((prev) => [...prev, title]);
+  };
+
+  useEffect(() => {
+    let newLastActions = [];
+
+    selectedFilters.forEach((item) => {
+      newLastActions = [...newLastActions, ...getLastActions(item)];
+    });
+
+    if (newLastActions.length === 0) newLastActions = undefined;
+
+    if (
+      JSON.stringify(newLastActions) ===
+      JSON.stringify(filterAndSearchText?.lastActions)
+    )
+      return;
+
+    setFilterAndSearchText((prev) => ({
+      ...prev,
+      lastActions: newLastActions,
+    }));
+  }, [
+    selectedFilters,
+    setFilterAndSearchText,
+    filterAndSearchText?.lastActions,
+  ]);
 
   return (
     <div className="w-full flex justify-between items-center px-4 py-3 bg-white rounded-xl shadow-[0_2px_8px_rgba(0,0,0,0.2)] hover:shadow-[0_4px_12px_rgba(0,0,0,0.3)]">
       <div className="flex items-center">
-        {filters.map((filter) => (
+        {taskStatuses.map((title) => (
           <FilterButton
-            key={filter}
-            isSelected={selectedFilter === filter}
-            onClick={() => setSelectedFilter(filter)}
-            title={filter}
+            key={title}
+            isSelected={selectedFilters.includes(title)}
+            onClick={() => handleFilterClick(title)}
+            title={title}
           />
         ))}
       </div>
@@ -44,8 +75,6 @@ const Filters = ({
         <input
           type="text"
           placeholder="Search tasks..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
           className="pl-10 pr-4 py-2 border border-gray-300 rounded-full text-sm focus:ring-2 focus:ring-[#282c34] focus:outline-none transition flex-1"
         />
         <Search size={16} className="absolute left-3 top-2.5 text-gray-400" />
--- frontend/src/utils/task.js
+++ frontend/src/utils/task.js
@@ -15,3 +15,18 @@ export const getTaskStatus = (lastAction) => {
       return TASK_STATUS.NOT_STARTED;
   }
 };
+
+export const getLastActions = (status) => {
+  switch (status) {
+    case TASK_STATUS.NOT_STARTED:
+      return [TASK_ACTIONS.CREATE];
+    case TASK_STATUS.IN_PROGRESS:
+      return [TASK_ACTIONS.START, TASK_ACTIONS.RESUME];
+    case TASK_STATUS.PAUSED:
+      return [TASK_ACTIONS.PAUSE];
+    case TASK_STATUS.COMPLETED:
+      return [TASK_ACTIONS.COMPLETE];
+    default:
+      return [];
+  }
+};
